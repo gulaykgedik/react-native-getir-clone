028d0a54a0b4874b6db9927f44739e8e9c93e684
Gülay Kızılgedik <164217083+gulaykgedik@users.noreply.github.com>
2025-11-27 20:43:52 +0300

    getir-clone

---
+++ src/screens/CategoryFilterScreen/index.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { ScrollView } from "react-native"
+
+import CategoryFiltering from "../../components/CategoryFiltering"
+import TypeFiltering from "../../components/TypeFiltering"
+import ProductsContainer from "../../components/ProductsContainer"
+
+import productsGetir from "../../../assets/productsGetir";
+function index(props: any) {
+    const [category, setCategory] = useState(props.route.params.category)
+    return (
+        <ScrollView stickyHeaderIndices={[0]} style={{ height: '100%', backgroundColor: '#f5f5f5' }}>
+            <CategoryFiltering category={category} />
+            <TypeFiltering/>
+            <ProductsContainer  />
+        </ScrollView>
+    )
+}
+export default index
